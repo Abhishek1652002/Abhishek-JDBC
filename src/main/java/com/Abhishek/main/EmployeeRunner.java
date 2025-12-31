c1343187f1abdcce3715bfcea93926d975c980a7
Abhishek1652002 <abhishekparas77@gmail.com>
2025-12-31 22:29:16 +0530

    Initial commit

---
+++ src/main/java/com/Abhishek/main/EmployeeRunner.java
@@ -0,0 +1,69 @@
+package com.Abhishek.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.Abhishek.dao.EmployeeDaoImpl;
+import com.Abhishek.entity.Employee;
+import com.Abhishek.dao.EmployeeDao;
+
+public class EmployeeRunner {
+
+	public static void main(String[] args) throws SQLException    {
+		
+		//Persistent object
+		
+		
+		EmployeeDao edao=new EmployeeDaoImpl();
+		
+		Employee emp=new Employee(16,"Anshika","female",77000);
+		
+		
+		//edao.saveEmp(emp);
+		
+		//edao.updateEmp(emp);
+		
+		edao.deleteEmpById(4);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root","root" );
+//		
+//		Statement statement =connection.createStatement();
+//		
+//		statement.executeUpdate("insert into employee(id,name,gender,salary) values("+ravi.getId()+",'"+ravi.getName()+"','"+ravi.getGender()+"',"+ravi.getSalary()+")");
+//		
+		
+		
+		System.out.println("Query Executed");
+	}
+
+}
+
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	
+
+
